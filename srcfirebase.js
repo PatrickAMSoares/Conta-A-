0072abd2b16f62707035686c393c4f9160400e10
PatrickAMSoares <patrickalmeidams@outlook.com>
2026-09-08 15:15:45 -0300

    Add files via upload

---
+++ srcfirebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCQ3mTMybqdkr0A-TcbxJWjYHEKcWy5nco",
+  authDomain: "conta-ai-a0d87.firebaseapp.com",
+  projectId: "conta-ai-a0d87",
+  storageBucket: "conta-ai-a0d87.firebasestorage.app",
+  messagingSenderId: "917747860067",
+  appId: "1:917747860067:web:3b4df49ef40f544ab92e56"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
